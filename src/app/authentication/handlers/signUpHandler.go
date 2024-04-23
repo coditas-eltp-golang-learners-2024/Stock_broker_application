d2d4cd82396328136c39dfdc7f5f1ec4146c475a
Nagesh <nageshnagansur@gmail.com>
2024-04-23 22:24:59 +0530

    Downgraded go version

--- src/app/authentication/handlers/signUpHandler.go
+++ src/app/authentication/handlers/signUpHandler.go
@@ -21,7 +21,7 @@ func SignUp(userSignUpService business.SignUpService) gin.HandlerFunc {
 			return
 		}
 		if err := userSignUpService.SignUp(&user); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
