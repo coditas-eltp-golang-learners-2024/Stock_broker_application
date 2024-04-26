1dfd9559b7f295bfd221373afc7fb24428859885
vipin-raj-coditas <vipinrajuk3@gmail.com>
2024-04-26 10:26:38 +0530

    Revert "updated repo"
    This reverts commit 154ec1ffd9a52b3c1b399f1e6baa915f4fa0ee1b.

--- src/app/authentication/commons/constants/errors.go
+++
@@ -1,8 +0,0 @@
-package constants
-
-import "errors"
-
-var (
-	ErrOtpVerification = errors.New("OTP verification failed")
-	ErrGenToken        = errors.New("failed to generate token")
-)
--- src/app/authentication/commons/constants/routes.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-// authentication route Constants
-const (
-	SwaggerRoute              = "/swagger/*any"
-	AuthenticationHealthCheck = "/authentication/health-check"
-	Metrics                   = "/metrics"
-	OTPValidationRoute        = "/validateOTP"
-)
