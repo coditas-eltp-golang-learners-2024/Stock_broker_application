1869dec2f6cc83d474103c92dafa862b91f26bb7
prahlad.singh-coditas <prahlad.singh@coditas.com>
2024-04-30 12:10:37 +0530

    dbModel users updated

--- src/models/dbModels.go
+++ src/models/dbModels.go
@@ -1,13 +1,24 @@
 package models
 
-import "stock_broker_application/src/constants"
+import (
+	"stock_broker_application/src/constants"
+	"time"
+)
 
-type ForgotPasswordRequest struct {
-	Email         string `gorm:"column:email" json:"email"`
-	PanCardNumber string `gorm:"column:pan_card" json:"pancardNumber"`
-	Password      string `gorm:"column:password" json:"NewPassword"`
+type Users struct {
+	Id             int       `gorm:"column:id" json:"id"`
+	UserName       string    `gorm:"column:username" json:"username"`
+	Name           string    `gorm:"column:name" json:"name"`
+	Email          string    `gorm:"column:email" json:"email"`
+	PhoneNumber    string    `gorm:"column:phone_number" json:"phone_number"`
+	PanCard        string    `gorm:"column:pan_card" json:"pan_card"`
+	Password       string    `gorm:"column:password" json:"password"`
+	Token          string    `gorm:"column:token" json:"token"`
+	CreationTime   time.Time `gorm:"column:created_at" json:"created_at"`
+	OTP            int       `gorm:"column:otp" json:"otp"`
+	EpochTimestamp int64     `gorm:"column:epochtimestamp" json:"epochtimestamp"`
 }
 
-func (ForgotPasswordRequest) TableName() string {
+func (Users) TableName() string {
 	return constants.UserTable
 }
