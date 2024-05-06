977e830bda961798cb43e32a0b0f346e0a7d609a
Nagesh <nageshnagansur@gmail.com>
2024-05-06 11:29:32 +0530

    Fixed issue

--- src/models/dbModels.go
+++ src/models/dbModels.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Users struct {
-	Id             int       `gorm:"column:id" json:"id"`
+	Id             int       `gorm:"primary_key;auto_increment" json:"id"`
 	UserName       string    `gorm:"column:username" json:"username"`
 	Name           string    `gorm:"column:name" json:"name"`
 	Email          string    `gorm:"column:email" json:"email"`
-	CreatedAt      time.Time `gorm:"column:created_at" json:"created_at"`
-	PhoneNumber    string    `gorm:"column:phone_number" json:"phone_number"`
-	PanCard        string    `gorm:"column:pan_card" json:"pan_card"`
+	CreatedAt      time.Time `gorm:"column:created_at" json:"createdAt"`
+	PhoneNumber    uint64    `gorm:"column:phone_number" json:"phoneNumber"`
+	PanCard        string    `gorm:"column:pan_card" json:"panCard"`
 	Password       string    `gorm:"column:password" json:"password"`
 	Token          string    `gorm:"column:token" json:"token"`
 	OTP            uint16    `gorm:"column:otp" json:"otp"`
