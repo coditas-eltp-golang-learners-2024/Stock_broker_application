59b79ba4ffff2d2023ef85fa3d932bec5cbd1282
Meghashree <megha.pawar2607@gmail.com>
2024-05-17 18:03:03 +0530

    PR Comment Resolved Given by Gauravi

--- src/app/watchlist/router/routes.go
+++ src/app/watchlist/router/routes.go
@@ -25,17 +25,14 @@ func GetRouter(middlewares ...gin.HandlerFunc) *gin.Engine {
 	router.Use(gin.Recovery())
 	connectionWithDb := postgres.GetPostGresClient().GormDb
 
-	//Create Watchlist
 	userDatabaseRepository := repositories.NewUserDBRepository(connectionWithDb)
 	createWatchlistHandler := business.NewCreateWatchlistService(userDatabaseRepository)
 	createWatchlistController := handler.NewWatchlistController(createWatchlistHandler)
 
-	//Get Watchlist
 	repository := repositories.NewGetWatclistsRepository(connectionWithDb)
 	service := business.NewUsersService(repository)
 	newGetwatchlistsController := handler.NewGetWatchListsController(service)
 
-	//Delete watchlist scrips
 	deleteWatchlistRepository := repositories.NewDeleteWatchlistRepository(connectionWithDb)
 	deleteWatchlistScripsService := business.NewDeleteWatchlistService(deleteWatchlistRepository)
 	deleteWatchlistScripsController := handler.NewDeleteWatchlistScripsController(deleteWatchlistScripsService)
@@ -50,11 +47,8 @@ func GetRouter(middlewares ...gin.HandlerFunc) *gin.Engine {
 		})
 		docs.SwaggerInfo.Schemes = []string{"http", "https"}
 		v1Routes.GET(serviceConstants.SwaggerRoute, ginSwagger.WrapHandler(swaggerFiles.Handler))
-
 		v1Routes.POST(serviceConstants.CreateWatchlist, headerCheck.AuthMiddleware(), createWatchlistController.HandleCreateWatchlist)
-
 		v1Routes.GET(serviceConstants.GetWatchLists, headerCheck.AuthMiddleware(), newGetwatchlistsController.HandleGetWatchlists)
-
 		v1Routes.DELETE(serviceConstants.DeleteWatchlistScrips, headerCheck.AuthMiddleware(), deleteWatchlistScripsController.HandleDeleteWatchlistScrips)
 	}
 	return router
